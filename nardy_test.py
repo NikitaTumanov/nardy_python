05cc5860d4ae780ce7617f3ce98922fae0306e33
NikitaTumanov <tuman0v.nik@yandex.ru>
2021-10-10 13:59:04 +0300

    first_test

---
+++ nardy_test.py
@@ -0,0 +1,20 @@
+import unittest
+import nardy
+
+
+class Test(unittest.TestCase):
+    def test_start(self):
+        a = nardy.Board()
+        a.start_game()
+        for i in range(0, 24):
+            if i != 0 and i != 12:
+                assert a.chess_arr[i] == ['', 0]
+            if i == 0:
+                assert a.chess_arr[i] == ['◉', 15]
+            if i == 12:
+                assert a.chess_arr[i] == ['○', 15]
+
+        if a.dice[0] > a.dice[1]:
+            assert a.turn == 'White'
+        else:
+            assert a.turn == 'Black'
