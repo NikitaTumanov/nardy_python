48bdf48a9890e665b5a92e644cd9180236b69233
NikitaTumanov <tuman0v.nik@yandex.ru>
2021-10-10 14:14:06 +0300

    role, change

--- nardy.py
+++ nardy.py
@@ -21,6 +21,20 @@ class Board(object):
             self.turn = 'Black'
         print('\n Dice:', self.dice, 'Start: {}'.format(self.turn))
 
+    def rollTheDice(self):
+        self.dice = []
+        self.dice.append(randint(1, 6))
+        self.dice.append(randint(1, 6))
+        self.outNumLeft = 1
+        if self.dice[0] == self.dice[1]:
+            self.dice.append(self.dice[0])
+            self.dice.append(self.dice[0])
+            self.outNumLeft = 2
+        return [self.dice]
+
+    def _ChangePlayer(self):
+        self.turn = 'White' if self.turn == 'Black' else 'Black'
+
     if __name__ == '__main__':
         board = Board()
         print('White & Black dices: ', end='')
